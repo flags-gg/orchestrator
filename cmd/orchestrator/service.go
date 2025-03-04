594e999b26c87eb392c37d7a9df17ff6afb90e2d
Keloran <keloran@develbox.info>
2025-03-04 21:00:48 +0000

    feat(config): add Clerk and Stripe keys to project config
    The changes in this commit add the Clerk and Stripe keys to the project configuration. This ensures that the necessary credentials are available for the Clerk and Stripe integrations in the orchestrator service.
    
    The key changes are:
    
    - Add the `ClerkKey` and `StripeSecret` properties to the `ProjectConfig` struct.
    - Update the `Build` method to set the `clerkKey` and `stripeKey` properties in the project configuration.
    - Remove the unused code related to fetching secrets from Vault.

--- cmd/orchestrator/service.go
+++ cmd/orchestrator/service.go
@@ -4,11 +4,7 @@ import (
 	"github.com/bugfixes/go-bugfixes/logs"
 	env "github.com/caarlos0/env/v8"
 	"github.com/flags-gg/orchestrator/internal"
-	vaulthelper "github.com/keloran/vault-helper"
-	"os"
-
 	ConfigBuilder "github.com/keloran/go-config"
-	ConfigVault "github.com/keloran/go-config/vault"
 )
 
 var (
@@ -27,10 +23,9 @@ func (pc ProjectConfig) Build(cfg *ConfigBuilder.Config) error {
 	}
 
 	type PC struct {
-		//ResendKey   string `env:"RESEND_KEY" envDefault:"flags-gg-resend-key"`
-		StripeLocal string `env:"STRIPE_LOCAL" envDefault:"stripe_local"`
-		//ClerkKey    string `env:"CLERK_SECRET_KEY" envDefault:"clerk_key"`
-		Flags FlagsService
+		StripeSecret string `env:"STRIPE_SECRET" envDefault:"stripe_secret"`
+		ClerkKey     string `env:"CLERK_SECRET_KEY" envDefault:"clerk_key"`
+		Flags        FlagsService
 	}
 	p := PC{}
 
@@ -40,72 +35,21 @@ func (pc ProjectConfig) Build(cfg *ConfigBuilder.Config) error {
 	if cfg.ProjectProperties == nil {
 		cfg.ProjectProperties = make(map[string]interface{})
 	}
-	//cfg.ProjectProperties["stripeLocal"] = p.StripeLocal
-	//cfg.ProjectProperties["clerkKey"] = p.ClerkKey
+	cfg.ProjectProperties["clerkKey"] = p.ClerkKey
+	cfg.ProjectProperties["stripeKey"] = p.StripeSecret
 
 	cfg.ProjectProperties["flags_agent"] = p.Flags.AgentID
 	cfg.ProjectProperties["flags_environment"] = p.Flags.EnvironmentID
 	cfg.ProjectProperties["flags_project"] = p.Flags.ProjectID
-
-	// get the resend key out of the vault
-	vh := *cfg.VaultHelper
-	if vh.Secrets() == nil {
-		return logs.Error("no secrets found")
-	}
-
-	//if cfg.ProjectProperties["resendKey"] == "" {
-	//	secret, err := vh.GetSecret("resend_key")
-	//	if err != nil {
-	//		return logs.Errorf("failed to get resend key: %v", err)
-	//	}
-	//	cfg.ProjectProperties["resendKey"] = secret
-	//}
-	//
-	//// get the clerkKey
-	//if cfg.ProjectProperties["clerkKey"] == "" {
-	//	secret, err := vh.GetSecret("clerk_key")
-	//	if err != nil {
-	//		return logs.Errorf("failed to get clerk key: %v", err)
-	//	}
-	//	cfg.ProjectProperties["clerkKey"] = secret
-	//}
 
 	return nil
 }
 
 func main() {
 	logs.Logf("Starting %s version %s (build %s)", ServiceName, BuildVersion, BuildHash)
-
-	kvPath := "kv/data/flags-gg/orchestrator"
-	if localPath, ok := os.LookupEnv("LOCAL_VAULT_PATH"); ok {
-		kvPath = localPath
-	}
-	vh := vaulthelper.NewVault("", "")
-	c := ConfigBuilder.NewConfig(vh)
-	c.VaultPaths = ConfigVault.Paths{
-		Database: ConfigVault.Path{
-			Credentials: "database/creds/flags_database",
-			Details:     kvPath,
-		},
-		Keycloak: ConfigVault.Path{
-			Details: kvPath,
-		},
-		Influx: ConfigVault.Path{
-			Details: kvPath,
-		},
-		BugFixes: ConfigVault.Path{
-			Details: kvPath,
-		},
-		Clerk: ConfigVault.Path{
-			Details: kvPath,
-		},
-		Resend: ConfigVault.Path{
-			Details: kvPath,
-		},
-	}
+	c := ConfigBuilder.NewConfigNoVault()
 
 	err := c.Build(
-		ConfigBuilder.Vault,
 		ConfigBuilder.Local,
 		ConfigBuilder.Database,
 		ConfigBuilder.Keycloak,
